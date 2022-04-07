258c6563e33881aa3b711a5ff8d51295423c834c
vishaldhuriya <vishaldhuriya90@gmail.com>
2022-04-07 22:32:56 +0530

    Create Test Git Project

---
+++ src/test/java/testpackage/Class.java
@@ -0,0 +1,34 @@
+package testpackage;
+
+public class Class {
+
+//	   *
+//	  ***
+//	 *****
+//	*******
+	
+	public static void main(String[] args)
+	{
+		int star=1;
+		int space =3;
+		
+		for(int i=1; i<=4; i++)
+		{
+			for(int j=1;j<=space;j++)
+			{
+				System.out.print(" ");
+			}
+			
+			for(int k=1;k<=star;k++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+			space--;
+			star=star+2;
+		}
+
+
+	}
+	
+}
